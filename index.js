b061fb753cac2b05af320c3b732846499830802e
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-12 17:06:20 -0300

    Adiciona função que muda o valor do municipio ao mudar o select do uf

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-(function(){
+(function () {
    $.ajax({
       type: "GET",
       url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-      
-      success: function (response) {
-         const data = response
-         for(let i = 0; i < data.length; i++){
-            $('#estado').append('<option>'+data[i].nome+'</option>')
-         }
-         
-      }
-   });
-})()
 
-function renderUf(){
-   const selectEstado = $('#estado').val()
+      success: function (response) {
+         const data = response;
+         for (let i = 0; i < data.length; i++) {
+            $("#estado").append("<option>" + data[i].nome + "</option>");
+         }
+      },
+   });
+})();
+
+function renderUf() {
+   const selectEstado = $("#estado").val();
    $.ajax({
       type: "GET",
       url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-      
+
       success: function (response) {
-         const data = response
-         for(let i = 0; i < data.length; i++){
-            if(data[i].nome == selectEstado){
-               $('#uf').val(data[i].sigla)
-            }
+         const data = response;
+         for (let i = 0; i < data.length; i++) {
+            $("#uf").append("<option>" + data[i].sigla + "</option>");
          }
-         
-      }
+      },
    });
 }
 
-const selectEstado = document.querySelector("#estado")
+function renderMunicipio() {
+   const selectUf = $("#uf").val();
+   $.ajax({
+      type: "GET",
+      url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectUf}/municipios`,
 
-selectEstado.addEventListener("change", () => renderUf())
+      success: function (response) {
+         const data = response;
+         let municipios = [];
+         for (let i = 0; i < data.length; i++) {
+            municipios.push($("<option>" + data[i].nome + "</option>"));
+         }
+         $("#municipio").html(municipios);
+      },
+   });
+}
 
+const selectEstado = document.querySelector("#estado");
+const selectUf = document.querySelector("#uf");
+selectEstado.addEventListener("change", () => renderUf());
+selectUf.addEventListener("change", () => renderMunicipio());
