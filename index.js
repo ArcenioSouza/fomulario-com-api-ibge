f9e27b4ebfc8b8474c913a423649a1cbb26564a1
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-04 14:41:48 -0300

    Transforma as funções get em Promise

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-(function renderEstado() {
-   $.ajax({
-      type: "GET",
-      url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-
-      success: function (response) {
-         const data = response;
-         for (let i = 0; i < data.length; i++) {
-            $("#estado").append("<option>" + data[i].nome + "</option>");
-         }
-      },
+function getState() {
+   return new Promise((resolve, reject) => {
+      resolve(
+         $.ajax({
+            type: "GET",
+            url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome",
+         })
+      )
    });
-})();
+};
 
-(function renderUf() {
-   const selectEstado = $("#estado").val();
-   $.ajax({
-      type: "GET",
-      url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-
-      success: function (response) {
-         const data = response;
-         for (let i = 0; i < data.length; i++) {
-            $("#uf").append("<option>" + data[i].sigla + "</option>");
-         }
-      },
+function getCounties() {
+   return new Promise((resolve, reject) => {
+      const selectUf = $("#uf").val();
+      resolve(         
+         $.ajax({
+            type: "GET",
+            url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectUf}/municipios`,
+         })
+      )
    });
-})();
+}
 
-function renderMunicipio() {
-   const selectUf = $("#uf").val();
-   $.ajax({
-      type: "GET",
-      url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectUf}/municipios`,
+getState()
+   .then((response) => {
+      const data = response;
+      data.map(estado => {
+         $("#estado").append("<option>" + estado.nome + "</option>");
+      })
+      /* for (let i = 0; i < data.length; i++) {
+         $("#estado").append("<option>" + data[i].nome + "</option>");
+      } */
+      for (let j = 0; j < data.length; j++) {
+         $("#uf").append("<option>" + data[j].sigla + "</option>");
+      }
+   })
+   .catch((error) => console.error('Erro: ', error.status, error.statusText))
 
-      success: function (response) {
+const selectUf = document.querySelector("#uf");
+
+selectUf.addEventListener("change", () => {
+   getCounties()
+      .then((response) => {
          const data = response;
          let municipios = [];
          for (let i = 0; i < data.length; i++) {
             municipios.push($("<option>" + data[i].nome + "</option>"));
          }
          $("#municipio").html(municipios);
-      },
-   });
-}
-
-const selectUf = document.querySelector("#uf");
-
-selectUf.addEventListener("change", () => renderMunicipio());
+      })
+      .catch((error) => console.error('Erro: ', error.status, error.statusText))
+});
